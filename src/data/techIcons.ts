07dc3becb4dca91a8f250e5ec3e479cd71f4aca0
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2026-05-12 17:51:03 +0200

    add all and roughly categorize tech icons for skills

--- src/data/techIcons.ts
+++ src/data/techIcons.ts
@@ -1,16 +1,47 @@
-import ReactIcon from "../assets/icons/react.svg";
-import TsIcon from "../assets/icons/typescript.svg";
-import CssIcon from "../assets/icons/css.svg";
+import React from "../assets/icons/react.svg";
+import TypeScript from "../assets/icons/typescript.svg";
+import Css from "../assets/icons/css.svg";
+import Tailwind from "../assets/icons/tailwindcss.svg";
+import Typeorm from "../assets/icons/typeorm.svg";
+import Redux from "../assets/icons/redux.svg";
+import ReactNative from "../assets/icons/react-native.svg";
+import Postgresql from "../assets/icons/postgresql.svg";
+import Nodejs from "../assets/icons/nodejs.svg";
+import Nestjs from "../assets/icons/nestjs.svg";
+import MongoDb from "../assets/icons/mongodb.svg";
+import Html5 from "../assets/icons/html5.svg";
+import Figma from "../assets/icons/figma.svg";
+import Jest from "../assets/icons/jest.svg";
+import Axios from "../assets/icons/axios.svg";
+import Atlassian from "../assets/icons/atlassian.svg";
+import Expo from "../assets/icons/expo.svg";
 
 // "lookup" table
 export const techIconMap: Record<string, string> = {
-  React: ReactIcon,
-  TypeScript: TsIcon,
-  CSS: CssIcon,
+  React: React,
+  TypeScript: TypeScript,
+  CSS: Css,
+  Tailwind: Tailwind,
+  TypeORM: Typeorm,
+  Redux: Redux,
+  PostgreSQL: Postgresql,
+  Nodejs: Nodejs,
+  NestJS: Nestjs,
+  MongoDB: MongoDb,
+  MongoDb: MongoDb,
+  HTML5: Html5,
+  Figma: Figma,
+  Jest: Jest,
+  Axios: Axios,
+  Atlassian: Atlassian,
+  "Tailwind CSS": Tailwind,
+  "React Native": ReactNative,
+  "Node.js": Nodejs,
+  "Expo Router": Expo,
 };
 
 // skills section tech
-export const frontendTech = ["React", "TypeScript", "CSS"];
-export const uxuiTech = ["React", "TypeScript"];
-export const extraTech = ["TypeScript", "CSS"];
-export const databasesTech = ["React", "CSS"];
+export const frontendTech = ["React", "TypeScript", "HTML5", "CSS", "Tailwind", "Redux", "React Native", "Expo Router"];
+export const uxuiTech = ["Figma", "CSS", "Tailwind"];
+export const extraTech = ["Jest", "Axios", "Atlassian"];
+export const databasesTech = ["PostgreSQL", "MongoDB", "TypeORM"];
