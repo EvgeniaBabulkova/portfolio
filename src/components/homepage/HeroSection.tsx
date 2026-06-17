345d1332b0675064a5fc095db35b13018efdb5eb
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2026-06-17 14:50:12 +0200

    responsive hero - keeping left container

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -12,10 +12,14 @@ export default function HeroSection() {
           <span> hello hello hello</span>
         </div>
 
-        <div className="flex flex-col gap-(--spacing-md) m-(--spacing-xl) mr-(--spacing-3xl)">
-          <h3>I’m Eva, Frontend developer with UX/UI design background</h3>
+        <div className={heroStyles.leftContent}>
+          <h3>
+            I’m Eva, <span className="text-(--col-text-secondary)">Frontend developer</span> with UX/UI design
+            background
+          </h3>
           <h5>experienced in building scalable React + TypeScript applications</h5>
           <LinkButton link={resumeLink}>View Resume</LinkButton>
+          <img className={heroStyles.mobilePh} src={Eva} alt="Evgenia Babulkova" />
         </div>
       </div>
 
@@ -25,9 +29,6 @@ export default function HeroSection() {
           <span> hello hello hello</span>
         </div>
 
-        <h5 className={heroStyles.mobileIntro}>
-          I’m Eva, <span className="text-(--col-text-primary)">Frontend developer</span> with UX/UI design background
-        </h5>
         <img src={Eva} alt="Evgenia Babulkova" />
       </div>
     </section>
